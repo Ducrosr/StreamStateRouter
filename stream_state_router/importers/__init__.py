b88e3d1c8087e7a6eed8d5a5f7d853b6517e5273
Ducros Rémy <155925139+Ducrosr@users.noreply.github.com>
2026-09-22 15:46:48 +0200

    Add OBS scene collection importer

---
+++ stream_state_router/importers/__init__.py
@@ -0,0 +1,17 @@
+from .advanced_scene_switcher import (
+    AdvancedSceneSwitcherImportReport,
+    AdvancedSceneSwitcherImporter,
+)
+from .scene_collection import (
+    CollectionImportReport,
+    SceneCollectionImporter,
+    SceneCollectionSnapshot,
+)
+
+__all__ = [
+    "AdvancedSceneSwitcherImportReport",
+    "AdvancedSceneSwitcherImporter",
+    "CollectionImportReport",
+    "SceneCollectionImporter",
+    "SceneCollectionSnapshot",
+]
