7ad3b6ea6830939272e82c9cd53ed280a87677f8
Ducros Rémy <155925139+Ducrosr@users.noreply.github.com>
2026-09-22 16:06:16 +0200

    Add OBS collection and Advanced Scene Switcher importer

---
+++ stream_state_router/importing/__init__.py
@@ -0,0 +1,15 @@
+from .obs_collection import (
+    CollectionImportPlan,
+    ImportedMacro,
+    OBSCollectionImporter,
+    apply_collection_import_plan,
+    find_obs_collection_file,
+)
+
+__all__ = [
+    "CollectionImportPlan",
+    "ImportedMacro",
+    "OBSCollectionImporter",
+    "apply_collection_import_plan",
+    "find_obs_collection_file",
+]
