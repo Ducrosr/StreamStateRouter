a2d5be15e4f5183b21556288b9a5a55fc887213f
Ducros Rémy <155925139+Ducrosr@users.noreply.github.com>
2026-09-22 16:38:57 +0200

    Add OBS collection and Advanced Scene Switcher importer

---
+++ stream_state_router/importing/__init__.py
@@ -0,0 +1,15 @@
+from .collection import (
+    CollectionImportPreview,
+    OBSCollectionImporter,
+    apply_collection_import,
+    convert_advss_payload,
+    find_obs_collection_json,
+)
+
+__all__ = [
+    "CollectionImportPreview",
+    "OBSCollectionImporter",
+    "apply_collection_import",
+    "convert_advss_payload",
+    "find_obs_collection_json",
+]
